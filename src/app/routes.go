d4d3c103dab5dc7af64e4a6ce632da92ee196f19
David Ilenwabor <dave11ilen@gmail.com>
2019-12-08 01:41:15 +0100

    Testing app

--- src/app/routes.go
+++ src/app/routes.go
@@ -3,10 +3,14 @@ package main
 import (
 	"github.com/Demistry/Hotel-Management-System/src/controllers/admin"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 var adminRoutes *mux.Router
 func addRoutes(){
+	GlobalRouter.HandleFunc("/", func(resp http.ResponseWriter, r *http.Request){
+		_, _ = resp.Write([]byte("WELCOME TO HOTSYS APP"))
+	})
 	adminRoutes = GlobalRouter.PathPrefix("/admin").Subrouter()
 	adminRoutes.HandleFunc("/create",admin.CreateNewHotelAdmin).Methods("POST")
 }
